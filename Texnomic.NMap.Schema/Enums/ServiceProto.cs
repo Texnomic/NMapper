c3fe692950e7dc237ac63b22ec035209a5d042e0
Matt Michielsen <matt@michielsen.us>
2022-03-01 03:15:33 +0000

    Added missing ServiceProto enum

---
+++ Texnomic.NMap.Schema/Enums/ServiceProto.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace Texnomic.NMap.Schema.Enums
+{
+    [XmlType("ServiceProto")]
+    public enum ServiceProto
+    {
+        [XmlEnum("rpc")]
+        Rpc,
+    }
+}
